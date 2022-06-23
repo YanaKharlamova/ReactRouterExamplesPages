d73ae9e2f1c03373c49457f8897bbfd99317b3f3
Yana <petrakharlamowa@gmail.com>
2022-06-23 17:56:53 +0300

    main: 1st commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 // import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
